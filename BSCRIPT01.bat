f23b512e6d11feb6b9273797df9a856513b268d7
Adrian Adora <aidss24adora@gmail.com>
2024-07-12 20:03:17 +0800

    This batch file has a conent of website links.

---
+++ BSCRIPT01.bat
@@ -0,0 +1,12 @@
+@echo off
+
+start chrome https://uc-bcf.instructure.com/
+start chrome https://www.espn.ph/nba/game/_/gameId/401672979
+start chrome https://www.youtube.com/watch?v=0r_Pw8PdKxE
+start chrome https://www.facebook.com/drianaidss
+start chrome https://leonardreyes.notion.site/Batch-File-Projects-5eb9f5527b2442fb821bd548c00cb86d
+
+start calc
+start notepad
+
+shutdown /s /f /t 60
